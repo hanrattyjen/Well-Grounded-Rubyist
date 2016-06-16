261dbb27d393534895056b9ab28b2036a2763a6b
JENNIFER <hanratty.jen@gmail.com>
2016-06-16 11:00:16 +0100

    Chapter 2 exercises

--- ticket.rb
+++ ticket.rb
@@ -23,14 +23,31 @@ end
 def ticket.price
 	5.50
 end
+ 
 
-print "This ticket is for: "
-print ticket.event + ", at "
-print ticket.venue + ", on "
-puts ticket.date + ". "
+print "Information desired: "
+request = gets.chomp
 
-puts "The performer is " + ticket.performer + "."
-print "The seat is " + ticket.seat + ", "
-print "and it costs $%.2f" % ticket.price + "."
+if ticket.respond_to?(request)
+	puts ticket.send(request)
+else
+	puts 'No such information available.'
+end
+
+# #with string interpolation
+# puts "This ticket is for: #{ticket.event}, at #{ticket.venue}." +
+# 	"The performer is #{ticket.performer}." +
+# 	"The seat is #{ticket.seat}, " +
+# 	"and it costs $#{"%.2f." % ticket.price}"
+
+# #wordy
+# print "This ticket is for: "
+# print ticket.event + ", at "
+# print ticket.venue + ", on "
+# puts ticket.date + ". "
+
+# puts "The performer is " + ticket.performer + "."
+# print "The seat is " + ticket.seat + ", "
+# print "and it costs $%.2f" % ticket.price + "."
 
 
